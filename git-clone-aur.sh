2a0179ecfb3992cbd8111e82296ab3b5aa044749
Bryan Lai <bryanlais@gmail.com>
2023-08-09 01:13:33 +0800

    Update script: prepare commit to be forked

--- git-clone-aur.sh
+++ git-clone-aur.sh
@@ -4,13 +4,27 @@ set -x
 
 BRANCH=$1
 REPO=$2
+UPSTREAM=$3
 
 if [[ -z $1 ]]; then bat "$0"; exit; fi
 [[ -z $2 ]] && REPO=bryango/aur
+[[ -z $2 ]] && UPSTREAM=archlinux/aur
 
-git clone \
+target="$(basename "$REPO")-$(basename "$BRANCH")"
+origin="git@github.com:$REPO.git"
+upstream="git@github.com:$UPSTREAM.git"
+
+if git clone \
     --filter=blob:none \
     --branch="$BRANCH" \
     --single-branch \
-    "git@github.com:$REPO.git" \
-    "$(basename "$REPO" | sed -e 's/^arch-//g')-$(basename "$BRANCH")"
+    "$origin" "$target"
+then
+    exit
+fi
+
+## otherwise,
+set -xe
+commit=$(git ls-remote "$upstream" "$BRANCH" | cut -f1)
+echo "$BRANCH"
+open "https://github.com/$REPO/tree/$commit"
